250cfa3ebb61b77271091e9c2d5c458482fd4a4a
¨gigihirao¨ <gigihirao@gmail.com>
2018-09-30 19:58:31 -0300

    removido exemplo de json e descomentado a funcao de chamar a api

--- dist/app.js
+++ dist/app.js
@@ -6,51 +6,8 @@ async function getImage(){
     return
   }
   for(var i = 0 ; i < imageID.length ; i++){
-    // response = await fetch("https://api.foursquare.com/v2/venues/" + imageID[i] + "/photos?client_id=EL0HV0FHVY0GUKYDBXJIACLRQTQ43TD15QJCZSLP2IGTNTCU&client_secret=WJVP5G4QZWIVQGFU41AZ2TFR51QVVW23PTV3PW10QXOPU15E&v=20180323&limit=10");
-    // route = await response.json();
-    let route = {
-      "meta": {
-          "code": 200,
-          "requestId": "5bb16ece4c1f6750c62c62cb"
-      },
-      "response": {
-          "photos": {
-              "count": 1,
-              "items": [
-                  {
-                      "id": "51610fdde4b0b4745db451ba",
-                      "createdAt": 1365315549,
-                      "source": {
-                          "name": "Instagram",
-                          "url": "http://instagram.com"
-                      },
-                      "prefix": "https://igx.4sqi.net/img/general/",
-                      "suffix": "/3564704_OI7_Y-_sVAwbOxpnMybWmX_Ip3QgyOxez00ef3lOY4g.jpg",
-                      "width": 612,
-                      "height": 612,
-                      "user": {
-                          "id": "3564704",
-                          "firstName": "Phillip",
-                          "lastName": "Alnswick-Tobias",
-                          "gender": "male",
-                          "photo": {
-                              "prefix": "https://igx.4sqi.net/img/user/",
-                              "suffix": "/QANG0KPDYAJ2Z0H5.jpg"
-                          }
-                      },
-                      "checkin": {
-                          "id": "51610fdce4b0e58eee1149e4",
-                          "createdAt": 1365315548,
-                          "type": "checkin",
-                          "timeZoneOffset": -240
-                      },
-                      "visibility": "public"
-                  }
-              ],
-              "dupesRemoved": 0
-          }
-      }
-  }
+    response = await fetch("https://api.foursquare.com/v2/venues/" + imageID[i] + "/photos?client_id=EL0HV0FHVY0GUKYDBXJIACLRQTQ43TD15QJCZSLP2IGTNTCU&client_secret=WJVP5G4QZWIVQGFU41AZ2TFR51QVVW23PTV3PW10QXOPU15E&v=20180323&limit=10");
+    route = await response.json();
     route.response.photos.items.forEach(function(item){
       image.push(item.prefix + "300" + item.suffix);
     });
